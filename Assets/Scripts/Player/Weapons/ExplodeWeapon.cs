3694a5db3e4a959d860b20ee11bccece9e638e02
zhangchen <zhangchen02@corp.netease.com>
2021-07-17 22:22:47 +0800

    [DEV]流星锤根据力变化威力

---
+++ Assets/Scripts/Player/Weapons/ExplodeWeapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeWeapon : MonoBehaviour
+{
+    [SerializeField] float limitMagnitude = 2;
+    [SerializeField] float powerScale = 100;//根据速度
+    [SerializeField] LayerMask shardLayers;
+    [SerializeField] AudioSource autio;
+
+    //发生碰撞
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude < limitMagnitude) return;
+        var breakAble = collision.collider.gameObject.GetComponent<BreakOnTime>();
+        bool canBreak = breakAble != null;
+        if (canBreak)
+        {
+            breakAble.SetSubdivide();
+        }
+        
+       if (autio) autio.Play();
+        ContactPoint contact = collision.contacts[0];
+        Vector3 pos = contact.point;
+        Explode(collision.relativeVelocity.magnitude * powerScale, pos);
+    }
+
+    public void Explode(float power, Vector3 position)
+    {
+        foreach (var shard in Physics.OverlapSphere(position, 1, shardLayers))
+        {
+            var rb = shard.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.AddExplosionForce(power, position, 1);
+            }
+        }
+    }
+}
--- Assets/Scripts/Test/AutoAddForce.cs
+++ Assets/Scripts/Test/AutoAddForce.cs
@@ -9,7 +9,7 @@ public class AutoAddForce : MonoBehaviour
 {
     [SerializeField] Vector3 dir;
     [SerializeField] float timeInterval = 1;
-    [SerializeField] LayerMask shardLayers;
+    
     private float time;
     private Rigidbody rigidbody;
     // Start is called before the first frame update
@@ -29,33 +29,6 @@ public class AutoAddForce : MonoBehaviour
         }
     }
 
-    //发生碰撞
-    private void OnCollisionEnter(Collision collision)
-    {
-        var breakAble = collision.collider.gameObject.GetComponent<BreakOnTime>();
-        bool canBreak = breakAble != null;
-         if (canBreak)
-        {
-            breakAble.SetSubdivide();
-        }
-
-        ContactPoint contact = collision.contacts[0];
-        Vector3 pos = contact.point;
-        Explode(1000, pos);
-    }
-
-    public void Explode(float power, Vector3 position)
-    {
-        foreach (var shard in Physics.OverlapSphere(position, 1, shardLayers))
-        {
-            var rb = shard.GetComponent<Rigidbody>();
-           
-            if (rb != null)
-            {
-                rb.isKinematic = false;
-                rb.AddExplosionForce(power, position, 1);
-            }
-        }
-    }
+ 
 
 }
