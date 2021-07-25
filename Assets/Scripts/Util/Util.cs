f2d5a8ced4750898779d305f13a147807bdfb010
zhangchen <zhangchen02@corp.netease.com>
2021-07-25 16:18:18 +0800

    [DEV]增加Puppet的抓取碰撞器，细节还需设计

---
+++ Assets/Scripts/Util/Util.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 一些通用方法
+/// </summary>
+public static class LayerUtil
+{
+    /// <summary>
+    /// 是否属于这个层
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="layerMask"></param>
+    /// <returns></returns>
+    public static bool IsInLayerMask(GameObject obj, LayerMask layerMask)
+    {
+        int objLayerMask = 1 << obj.layer;
+        return (layerMask.value & objLayerMask) > 0;
+    }
+}
