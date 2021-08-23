1f1e4c1cbd46fba395d26868697a38badd3b55e7
zhangchen <zhangchen02@corp.netease.com>
2021-08-23 22:01:47 +0800

    [DEV]演示案例

--- Assets/Scripts/Common/SceneMainManager.cs
+++ Assets/Scripts/Common/SceneMainManager.cs
@@ -11,6 +11,8 @@ public class SceneMainManager : MonoBehaviour
     public enum ScenePath
     {
         Demo = 0,
+        Demo1 = 1,
+        Max = 2,
     }
     [Serializable]
     public class InputConfig
@@ -21,21 +23,31 @@ public class SceneMainManager : MonoBehaviour
     [Tooltip("设置场景切换的操控按钮")]
     [SerializeField]
     private InputConfig _inputConfig;
+
+    private int curScene = 0;//
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     // Update is called once per frame
     void Update()
     {
         if (OVRInput.GetDown(_inputConfig.buttonReset))
         {
-            LoadScene(ScenePath.Demo);
+            LoadNextScene();
         }
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            LoadScene(ScenePath.Demo);
+            LoadNextScene();
         }
     }
 
-    private void LoadScene(ScenePath scene)
+    private void LoadNextScene()
     {
-        SceneManager.LoadScene((int)scene);
+        ++curScene;
+        if (curScene == (int)ScenePath.Max) curScene = 0;
+        Debug.Log($"Loading scene :{(ScenePath)curScene}");
+        SceneManager.LoadScene(curScene);
     }
 }
